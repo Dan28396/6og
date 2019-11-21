60285d2b7d8ba1aacefc7bb26c42de8d650c4622
Dan28396 <Dan24335>
2019-11-22 02:59:59 +0300

    CheckoutPage fix

--- src/components/Checkout/CheckoutPage.vue
+++ src/components/Checkout/CheckoutPage.vue
@@ -1,18 +1,120 @@
 <template>
     <main>
-        <PayPalButton/>
+        <section class="checkout">
+            <div class="checkout__content-wrap">
+                <div class="checkout__section">
+                    <h2 class="section-title">Contact information</h2>
+                    <input class="input-wrap__input" placeholder="Email">
+                </div>
+                <div class="checkout__section">
+                    <h2 class="section-title">Shipping address</h2>
+                    <div class="section__name">
+                        <input class="input-wrap__input" placeholder="First name">
+                        <input class="input-wrap__input" placeholder="Last name">
+                    </div>
+                    <input class="input-wrap__input" placeholder="Address">
+                    <input class="input-wrap__input" placeholder="City">
+                    <div class="section__address">
+                        <input class="input-wrap__input" placeholder="Country">
+                        <input class="input-wrap__input" placeholder="Region">
+                        <input class="input-wrap__input" placeholder="Postal code">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="checkout-cart">
+
+        </section>
     </main>
 </template>
 
 <script>
-    import PayPalButton from "@/components/Checkout/PayPalButton";
+
 
     export default {
         name: "CheckoutPage",
-        components: {PayPalButton}
+        components: {}
     }
 </script>
 
 <style scoped>
 
+    main {
+        width: 100%;
+        height: 100%;
+        min-height: 100vh;
+        display: flex;
+    }
+
+    .checkout {
+        width: 55%;
+        height: 100%;
+        min-height: 100vh;
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    .checkout__content-wrap {
+        width: 60%;
+        margin: 64px auto;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .checkout__section {
+        width: 100%;
+        margin-bottom: 30px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .section-title {
+        font-size: 18px;
+        color: #333333;
+        text-align: left;
+        margin-bottom: 20px;
+    }
+
+    .section__name {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        column-gap: 10px;
+    }
+    .section__address {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        column-gap: 10px;
+    }
+
+    .input-wrap__input {
+        border: 1px #d9d9d9 solid;
+        background-clip: padding-box;
+        border-radius: 5px;
+        display: block;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        width: 100%;
+        padding: 0.92857em 0.78571em;
+        word-break: normal;
+        line-height: inherit;
+        max-height: 45px;
+        font-size: 12px;
+        transition: all 0.2s ease-out;
+        outline: none;
+        margin-bottom: 15px;
+    }
+
+    .input-wrap__input:focus {
+        transition: all 0.2s ease-out;
+        border: 1px #101010 solid;
+    }
+
+    .checkout-cart {
+        width: 45%;
+        height: 100%;
+        background: #101010;
+        min-height: 100vh;
+    }
 </style>
--- src/components/Checkout/PayPalButton.vue
+++ src/components/Checkout/PayPalButton.vue
@@ -13,7 +13,7 @@
         mounted: function () {
             const script = document.createElement("script");
             script.src =
-                "https://www.paypal.com/sdk/js?client-id=Abgq28sebvqxUYG4ZS_48U6CSzNMcagv0wIrHcVqAI8EiRn1-KSFbr7e7GI0zw7_DiuvtxZfTNI58xFf&currency=RUB";
+                "https://www.paypal.com/sdk/js?client-id=Abgq28sebvqxUYG4ZS_48U6CSzNMcagv0wIrHcVqAI8EiRn1-KSFbr7e7GI0zw7_DiuvtxZfTNI58xFf&currency=USD";
             script.addEventListener("load", this.setLoaded);
             document.body.appendChild(script);
         },
@@ -30,20 +30,23 @@
                                     shipping_preference: "SET_PROVIDED_ADDRESS",
                                 },
                                 payer: {
-                                    address: {
-                                        address_line_1: "2211 N First Street",
-                                        address_line_2: "Building 17",
-                                        admin_area_2: "San Jose",
-                                        admin_area_1: "CA",
-                                        postal_code: "95131",
-                                        country_code: "US"
-                                    }
+                                    name:{
+                                        given_name: "Daniil",
+                                        surname: "Lobchenko"
+                                    },
+                                    email_address: 'lobchenko1999@gmail.com'
                                 },
                                 purchase_units: [
                                     {
                                         amount: {
-                                            currency_code: 'RUB',
-                                            value: total
+                                            currency_code: 'USD',
+                                            value: total,
+                                            breakdown: {
+                                                item_total: {
+                                                    currency_code: 'USD',
+                                                    value: '40'
+                                                }
+                                            }
                                         },
                                         shipping: {
                                             address: {
@@ -54,8 +57,26 @@
                                                 postal_code: "95131",
                                                 country_code: "US"
                                             }
-                                        }
-
+                                        },
+                                        items: [
+                                            {
+                                                name: "T-Shirt 6og",
+                                                unit_amount: {
+                                                    currency_code: "USD",
+                                                    value: '10'
+                                                },
+                                                quantity: '2'
+                                            },
+                                            {
+                                                name: "Sweatshirt 6og",
+                                                unit_amount: {
+                                                    currency_code: "USD",
+                                                    value: '10'
+                                                },
+                                                quantity: '2'
+                                            }
+                                        ],
+                                        description: 'Dostavit do 23.11'
                                     }
                                 ],
                             });
