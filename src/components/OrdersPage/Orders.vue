7eaf72d0a00c8e574438457148146a26669b6219
Dan28396 <Dan24335>
2020-02-09 20:09:51 +0300

    almost done

---
+++ src/components/OrdersPage/Orders.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <!--Сюда какать хтмл-->
+    </main>
+</template>
+
+<script>
+
+    // Здесь соответственно скрипты
+    export default {
+        name: "Orders"
+    }
+</script>
+
+<style scoped>
+    /*Здесь стили для данной хтмл*/
+</style>
